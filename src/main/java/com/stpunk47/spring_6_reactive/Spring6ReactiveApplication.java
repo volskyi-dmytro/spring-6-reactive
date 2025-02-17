8767e498b21e7c22329f103b95a6ffeef654b625
@stpunk47 <stpunk47@gmail.com>
2025-02-18 00:14:35 +0200

    Initial commit

---
+++ src/main/java/com/stpunk47/spring_6_reactive/Spring6ReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6ReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stpunk47/spring_6_reactive/Spring6ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
