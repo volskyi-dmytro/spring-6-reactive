015d130fa7e665afd2b091b0cab918bdbd6c8629
@stpunk47 <stpunk47@gmail.com>
2025-02-18 23:05:25 +0200

    Added WebFlux Controller and simple DTO.

---
+++ src/main/java/com/stpunk47/spring_6_reactive/controllers/BeerController.java
@@ -0,0 +1,21 @@
+package com.stpunk47.spring_6_reactive.controllers;
+
+import com.stpunk47.spring_6_reactive.model.BeerDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class BeerController {
+
+    public static final String BEER_PATH = "/api/v2/beer";
+
+
+    @GetMapping(BEER_PATH)
+    Flux<BeerDTO> listBeers(){
+
+        return Flux.just(BeerDTO.builder().id(1).build(),
+                BeerDTO.builder().id(2).build());
+    }
+
+}
---
+++ src/main/java/com/stpunk47/spring_6_reactive/model/BeerDTO.java
@@ -0,0 +1,30 @@
+package com.stpunk47.spring_6_reactive.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
+}
